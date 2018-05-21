a76d43c3d19c24c101b0b8c2b104c5198f89f6c2
Tsj <thibaultingargiola@gmail.com>
2018-05-21 18:31:26 +0200

    Fix camera controls

--- app/index.js
+++ app/index.js
@@ -1,4 +1,5 @@
 const THREE = require('three.js');
+const OrbitControls = require('three-orbitcontrols');
 
 var container;
 var camera, scene, renderer;
@@ -15,8 +16,9 @@ function init() {
     camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000);
     camera.position.set(500, 800, 1300);
     camera.lookAt(new THREE.Vector3());
+
     scene = new THREE.Scene();
-    scene.background = new THREE.Color(0xf0f0f0);
+    scene.background = new THREE.Color(0xfff0f0);
     // roll-over helpers
     var rollOverGeo = new THREE.BoxGeometry(50, 50, 50);
     rollOverMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000, opacity: 0.5, transparent: true });
@@ -24,7 +26,7 @@ function init() {
     scene.add(rollOverMesh);
     // cubes
     cubeGeo = new THREE.BoxGeometry(50, 50, 50);
-    cubeMaterial = new THREE.MeshLambertMaterial({ color: 0xfeb74c, map: new THREE.TextureLoader().load("textures/square-outline-textured.png") });
+    cubeMaterial = new THREE.MeshLambertMaterial({ color: 0xfeb74c });
     // grid
     var gridHelper = new THREE.GridHelper(1000, 50);
     scene.add(gridHelper);
@@ -42,13 +44,33 @@ function init() {
     var directionalLight = new THREE.DirectionalLight(0xffffff);
     directionalLight.position.set(1, 0.75, 0.5).normalize();
     scene.add(directionalLight);
+
+    /// Render
     renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
     container.appendChild(renderer.domElement);
+
+    var controls = new OrbitControls( camera, document );
+    controls.enableDamping = true;
+    controls.dampingFactor = 0.25;
+    controls.rotateSpeed = 0.35;
+    controls.enableKeys = true;
+    controls.enablePan = true;
+    controls.keys = {
+        LEFT: 37, //left arrow
+        UP: 38, // up arrow
+        RIGHT: 39, // right arrow
+        BOTTOM: 40 // down arrow
+    }
+    controls.mouseButtons = {
+        ORBIT: THREE.MOUSE.RIGHT,
+        PAN: THREE.MOUSE.MIDDLE
+    }
+
+    /// Listeners
     document.addEventListener('mousemove', onDocumentMouseMove, false);
     document.addEventListener('mousedown', onDocumentMouseDown, false);
-    document.addEventListener('mouseup', onDocumentMouseUp, false);
     document.addEventListener('keydown', onDocumentKeyDown, false);
     document.addEventListener('keyup', onDocumentKeyUp, false);
     document.addEventListener('wheel', onScroll, false);
@@ -62,9 +84,19 @@ function onWindowResize() {
 }
 function onDocumentMouseMove(event) {
     event.preventDefault();
-    if (isRightClickDown){
-        console.log(event);
+    if (isRightClickDown) {
+        /// control camera
+        if (event.ctrlKey) {
+            /// translate
+            camera.position.x -= event.movementX + event.movementY / 2;
+            camera.position.z -= event.movementY - event.movementX / 2;
+        } else {
+            /// rotate
+        }
+        camera.updateMatrix();
+        render();
     } else {
+        /// move shadow block
         mouse.set((event.clientX / window.innerWidth) * 2 - 1, - (event.clientY / window.innerHeight) * 2 + 1);
         raycaster.setFromCamera(mouse, camera);
         var intersects = raycaster.intersectObjects(objects);
@@ -74,25 +106,13 @@ function onDocumentMouseMove(event) {
             rollOverMesh.position.divideScalar(50).floor().multiplyScalar(50).addScalar(25);
         }
         render();
-    } 
+    }
 }
 function onDocumentMouseDown(event) {
     event.preventDefault();
     switch (event.button) {
         case 0:
             onLeftClickDown(event);
-            break;
-        case 2:
-            onRightClickDown(event);
-            break;
-    }
-}
-
-function onDocumentMouseUp(event) {
-    event.preventDefault();
-    switch (event.button) {
-        case 2:
-            onRightClickUp(event);
             break;
     }
 }
@@ -113,7 +133,7 @@ function render() {
 
 function onScroll(event) {
     event.preventDefault();
-    camera.zoom -= event.deltaY/1000;
+    camera.zoom -= event.deltaY / 1000;
     if (camera.zoom < 0) camera.zoom = 0;
     camera.updateProjectionMatrix();
     render();
@@ -142,13 +162,4 @@ function onLeftClickDown(event) {
         render();
     }
 }
-
-function onRightClickDown(event) {
-    isRightClickDown = true;
-}
-
-function onRightClickUp(event) {
-    isRightClickDown = false;
-}
-
 
